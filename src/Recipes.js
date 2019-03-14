4636c79182b3a7b0cd587b4aa12bba55f140028f
Bolaji Olajide <bolaji.olajide@andela.com>
2019-03-15 00:51:52 +0100

    provide dynamic arguments in  apollo query component

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 
 const recipesQuery = gql`
-    {
-        recipes {
+    query recipes($vegetarian: Boolean!) {
+        recipes(vegetarian: $vegetarian) {
             id
             title
         }
     }
 `;
 
-export default function() {
-    return (
-        <Query query={recipesQuery}>
-            {({ data, loading, error }) => {
-              if (loading) return <p>Loading...</p>
-              if (error) return <p>Something went wrong. {error.message} </p>
-              return (
-                <ul>
-                  {data.recipes.map(({ id, title }) => <li key={id}>{title}</li>)}
-                </ul>
-              );
-            }}
-        </Query>
-    );
+export default class Recipes extends Component {
+    state = {
+        vegetarian: false
+    }
+
+    updateVegetarianStatus = ({ target: { checked} }) => {
+        this.setState({ vegetarian: checked });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={this.state.vegetarian}
+                        onChange={this.updateVegetarianStatus}
+                    />
+                </label>
+                <Query
+                    query={recipesQuery}
+                    variables={{ vegetarian: this.state.vegetarian }}
+                >
+                    {({ data, loading, error }) => {
+                    if (loading) return <p>Loading...</p>
+                    if (error) return <p>Something went wrong. {error.message} </p>
+                    return (
+                        <ul>
+                        {data.recipes.map(({ id, title }) => <li key={id}>{title}</li>)}
+                        </ul>
+                    );
+                    }}
+                </Query>
+            </Fragment>
+        );
+    }
 }
