46b14aa4a7a48944443f53f96594c210011733bb
Bolaji Olajide <bolaji.olajide@andela.com>
2019-03-15 15:00:29 +0100

    refetch data with apollo

--- src/Recipes.js
+++ src/Recipes.js
@@ -34,54 +34,58 @@ export default class Recipes extends Component {
                 <Query
                     query={recipesQuery}
                     variables={{ vegetarian: this.state.vegetarian }}
+                    pollInterval={3000}
                 >
-                    {({ data, loading, error }) => {
-                    if (loading) return <p>Loading...</p>
-                    if (error) return <p>Something went wrong. {error.message} </p>
-                    return (
-                        <ul>
-                        {data.recipes.map(({ id, title, isStarred }) => (
-                            <Fragment key={id}>
-                                <li key={id}>
-                                    {title}
-                                    <Mutation
-                                        mutation={updateRecipeStarredMutation}
-                                        refetchQueries={[
-                                            {
-                                                query: recipesQuery,
-                                                variables: { vegetarian: true }
-                                            },
-                                            {
-                                                query: recipesQuery,
-                                                variables: { vegetarian: false }
-                                            }
-                                        ]}
-                                        awaitRefetchQueries
-                                    >
-                                        {(updateRecipeStarred, { loading, error }) => (
-                                            <button
-                                                onClick={() => updateRecipeStarred({
-                                                    variables: {
-                                                        id,
-                                                        isStarred: !isStarred
-                                                    }
-                                                })}
-                                                className="star-btn"
-                                                disabled={loading}
-                                                style={{
-                                                    color: isStarred ? 'orange' : 'grey',
-                                                    animation: loading ? 'inflate 0.7s ease infinte alternate' : 'none'
-                                                }}
-                                            >
-                                                * { error && 'Failed to update'}
-                                            </button>
-                                        )}
-                                    </Mutation>
-                                </li>
+                    {({ data, loading, error, refetch }) => {
+                        if (loading) return <p>Loading...</p>
+                        if (error) return <p>Something went wrong. {error.message} </p>
+                        return (
+                            <Fragment>
+                                <ul>
+                                    {data.recipes.map(({ id, title, isStarred }) => (
+                                        <Fragment key={id}>
+                                            <li key={id}>
+                                                {title}
+                                                <Mutation
+                                                    mutation={updateRecipeStarredMutation}
+                                                    refetchQueries={[
+                                                        {
+                                                            query: recipesQuery,
+                                                            variables: { vegetarian: true }
+                                                        },
+                                                        {
+                                                            query: recipesQuery,
+                                                            variables: { vegetarian: false }
+                                                        }
+                                                    ]}
+                                                    awaitRefetchQueries
+                                                >
+                                                    {(updateRecipeStarred, { loading, error }) => (
+                                                        <button
+                                                            onClick={() => updateRecipeStarred({
+                                                                variables: {
+                                                                    id,
+                                                                    isStarred: !isStarred
+                                                                }
+                                                            })}
+                                                            className="star-btn"
+                                                            disabled={loading}
+                                                            style={{
+                                                                color: isStarred ? 'orange' : 'grey',
+                                                                animation: loading ? 'inflate 0.7s ease infinte alternate' : 'none'
+                                                            }}
+                                                        >
+                                                            * { error && 'Failed to update'}
+                                                        </button>
+                                                    )}
+                                                </Mutation>
+                                            </li>
+                                        </Fragment>
+                                    ))}
+                                </ul>
+                                <button onClick={() => refetch()}>Refresh Recipes</button>
                             </Fragment>
-                        ))}
-                        </ul>
-                    );
+                        );
                     }}
                 </Query>
             </Fragment>
